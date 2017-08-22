10f9f4fe70b01563c4143d962341561833a3b489
orlandocursocic <orlandocursocic@gmail.com>
2017-08-23 01:17:33 +0200

    Implementado formulario para actualizar Persona. Implementado formulario para eliminar persona. Añadida l

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
+  $(document).ready( function() {
   var app = new Vue({
   		el: '#maestro',
     		data: {
       		selected: '',
+          seen:{seenCreate: false, seenDelete: false, seenUpdate: false},
       		personas: [],
+          personaDel : {id: ''},
           personaPost : {nombre: '', apellido: '', edad: '' },
+          personaPut : {id: '', nombre: '', apellido: '', edad: ''},
           personaDetalle: {id: '', nombre: '', pellido: '', edad: ''}
       	},
-        
+
         methods: {
 
           mostrarDetalles: function(id)
@@ -15,7 +19,7 @@
             var _this = this;
             $.ajax(
             {
-              url : "http://10.60.23.12:50292/api/Personas/"+id,
+              url : "http://localhost:50292/api/Personas/" + id,
               type: "GET",
             })
             .done(function(data) {
@@ -24,17 +28,11 @@
               _this.personaDetalle.nombre = data.nombre;
               _this.personaDetalle.apellido = data.apellido;
               _this.personaDetalle.edad = data.edad;
+
             })
             .fail(function(data) {
                     alert( "error" );
                   });
-          },
-
-          limpiarCampos: function()
-          {
-            this.personaPost.nombre = '';
-            this.personaPost.apellido = '';
-            this.personaPost.edad = '';
           },
 
           crearPersona: function(event)
@@ -42,33 +40,149 @@
 
             var _this = this;
 
-            /*$.post( "http://10.60.23.12:62270/api/personas", this.personaPost )
-            .done(function(data) {
-              _this.personas = data;
-              alert( "Creado el usuario: " + data.Nombre + " " + data.Apellidos+ " edad " + data.Edad );
-            });*/
-
             $.ajax({
 
               type: "POST",
-              url: "http://10.60.23.12:50292/api/Personas",
+              url: "http://localhost:50292/api/Personas",
               data: _this.personaPost,
 
             })
             .done(function(data) {
 
-              _this.personas.push(data);
-              alert( "Creado el usuario: " + data.nombre + " " + data.apellido+ " edad " + data.edad + "id:" + data.id);
-              _this.limpiarCampos();
+              alert( "Creado el usuario -> " + "Id: " + data.id + " Nombre: " + data.nombre + " Apellidos: " + data.apellido + " Edad: " + data.edad);
+              _this.refreshList();
+              _this.limpiarCamposPost();
+              _this.limpiarCamposSeen();
+              _this.limpiarCamposDetalles();
           });
+        },
+
+        actualizarPersona: function(event)
+        {
+
+          var _this = this;
+
+          $.ajax({
+
+            type: "PUT",
+            url: "http://localhost:50292/api/Personas/" + _this.personaPut.id,
+            data: _this.personaPut,
+
+          })
+          .done(function(data) {
+
+            alert( "Se ha actualizado el usuario");
+            _this.refreshList();
+            _this.limpiarCamposPut();
+            _this.limpiarCamposSeen();
+            _this.limpiarCamposDetalles();
+        });
+      },
+
+      eliminarPersona: function(event)
+      {
+
+        var _this = this;
+
+        $.ajax({
+
+          type: "DELETE",
+          url: "http://localhost:50292/api/Personas/" + _this.personaDel.id,
+          data: _this.personaDel,
+
+        })
+        .done(function(data) {
+          alert( "Eliminado el usuario -> " + "Id: " + data.id + " Nombre: " + data.nombre + " Apellidos: " + data.apellido + " Edad: " + data.edad);
+          _this.refreshList();
+          _this.limpiarCamposDelete();
+          _this.limpiarCamposSeen();
+          _this.limpiarCamposDetalles();
+      });
+    },
+
+          refreshList: function()
+          {
+
+            var _this = this;
+            $.ajax(
+              {
+                url : "http://localhost:50292/api/Personas",
+                type: "GET",
+              })
+              .done(function(data) {
+                _this.personas = data;
+              })
+              .fail(function(data) {
+                      alert( "error" );
+                    });
+          },
+
+          seenCreateAux: function()
+          {
+              var _this = this;
+              _this.seen.seenCreate = !_this.seen.seenCreate;
+              _this.seen.seenUpdate = false;
+              _this.seen.seenDelete = false;
+          },
+
+          seenUpdateAux: function()
+          {
+              var _this = this;
+              _this.seen.seenUpdate = !_this.seen.seenUpdate;
+              _this.seen.seenCreate = false;
+              _this.seen.seenDelete = false;
+          },
+
+          seenDeleteAux: function()
+          {
+              var _this = this;
+              _this.seen.seenDelete = !_this.seen.seenDelete;
+              _this.seen.seenUpdate = false;
+              _this.seen.seenCreate = false;
+          },
+
+          limpiarCamposDetalles: function()
+          {
+            this.personaDetalle.id = '';
+            this.personaDetalle.nombre = '';
+            this.personaDetalle.apellido = '';
+            this.personaDetalle.edad = '';
+          },
+
+          limpiarCamposDelete: function()
+          {
+            this.personaDel.id = '';
+          },
+
+          limpiarCamposPost: function()
+          {
+            this.personaPost.nombre = '';
+            this.personaPost.apellido = '';
+            this.personaPost.edad = '';
+          },
+
+          limpiarCamposPut: function()
+          {
+            this.personaPut.id = '';
+            this.personaPut.nombre = '';
+            this.personaPut.apellido = '';
+            this.personaPut.edad = '';
+          },
+
+          limpiarCamposSeen: function()
+          {
+            this.seen.seenCreate = false;
+            this.seen.seenDelete = false;
+            this.seen.seenUpdate = false;
           }
+
         },
 
         mounted: function() {
           var _this = this;
           $.ajax(
             {
-              url : "http://10.60.23.12:50292/api/Personas",
+              url : "http://localhost:50292/api/Personas",
               type: "GET",
             })
             .done(function(data) {
@@ -79,6 +193,5 @@
                   });
 
         }
-        
   	});
-  
+  });
